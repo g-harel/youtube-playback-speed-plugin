50eb22f4cbcab14666b310b252df5c9b61f6bae2
Gabriel <pabombs@gmail.com>
2017-05-04 20:37:16 -0400

    added support for youtube's new webapp naviagtion

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ chrome.storage.sync.get('youtube-playback-speed', (value) => {
 
 chrome.storage.onChanged.addListener((changes) => {
     setPlayback(changes['youtube-playback-speed'].newValue)
-}); 
+});
+
+window.addEventListener('click', () => {
+    setTimeout(() => {
+        chrome.storage.sync.get('youtube-playback-speed', (value) => {
+            setPlayback(value['youtube-playback-speed']);
+        });
+    }, 1000);
+});
